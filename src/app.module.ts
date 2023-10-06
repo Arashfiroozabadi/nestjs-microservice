effc60c96a94797f1d1bbb16ef0142ba3ba66542
firoozabadi <arashfiroozabadii@gmail.com>
2023-10-06 12:35:19 +0330

    use swc and setup swagger

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+const envModule = ConfigModule.forRoot({
+  isGlobal: true,
+});
+
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [envModule, UsersModule],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { Transport, MicroserviceOptions } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const NODE_ENV = process.env.NODE_ENV;
   const app = await NestFactory.create(AppModule);
 
   app.connectMicroservice<MicroserviceOptions>({
@@ -11,8 +13,24 @@ async function bootstrap() {
   });
 
   await app.startAllMicroservices();
+
+  // setup swagger
+  if (NODE_ENV === 'development') {
+    const config = new DocumentBuilder()
+      .setTitle('Swagger API DOC')
+      .setVersion('1.0')
+      .addBearerAuth()
+      .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('doc', app, document);
+  }
+
   await app.listen(3001, '0.0.0.0');
 
   console.log(`Application is running on: ${await app.getUrl()}`);
+  if (NODE_ENV === 'development') {
+    console.log(`swagger doc is running on: ${await app.getUrl()}/doc`);
+  }
 }
+
 bootstrap();
